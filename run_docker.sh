19ebd5fb02ea05771b94c5ac8dc4e00d141e62f4
roees <roees@rnd-hub.com>
2020-01-30 15:09:04 +0200

    redirected all files to shared volume for easier maintainance and version control

--- run_docker.sh
+++ run_docker.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-
-
 SELF_PATH=`readlink -f $0 | rev | cut -d/ -f2- | rev`
 
-echo "SELF_PATH is $SELF_PATH"
+echo "### SELF_PATH is $SELF_PATH"
 
 xhost +
-docker run -it -v $SELF_PATH/../shared:/root/work/shared -v $SELF_PATH/../recordings:/root/work/recordings  -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority --net=host --env="QT_X11_NO_MITSHM=1" cartographerenv /bin/bash &
-
-sleep 2
+docker run -dit -v $SELF_PATH/../shared:/root/work/shared -v $SELF_PATH/../recordings:/root/work/recordings -v $SELF_PATH/CartographerFiles/:/root/work/CartographerFiles/ -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/root/.Xauthority --net=host --env="QT_X11_NO_MITSHM=1" cartographerenv /bin/bash
 
 CNTNAME=`docker ps --last 1 | tail -n 1 | awk '{print $NF}'`
 
-echo "CNTNAME is $CNTNAME"
+echo "### CNTNAME is $CNTNAME"
 
-docker cp $SELF_PATH/CartographerFiles/urdf/StructurePicoDownKit.urdf $CNTNAME:/root/catkin_ws/install_isolated/share/cartographer_ros/urdf/StructurePicoDownKit.urdf
+docker cp $SELF_PATH/../lidar2tof.py $CNTNAME:/root/catkin_ws/src/scripts/src/lidar2tof.py
 
-docker cp $SELF_PATH/CartographerFiles/launch/demo_depth_camera.launch $CNTNAME:/root/catkin_ws/install_isolated/share/cartographer_ros/launch/demo_depth_camera.launch
+docker attach $CNTNAME
 
-#docker exec -it $CNTNAME /bin/bash
-fg
+# roslaunch /root/work/CartographerFiles/launch/demo_depth_camera.launch bag_filename:=/root/work/recordings/CartoIn.bag
